407e6485c2b0752741aa998c828f6377e56c2a3d
Belaka <creationforge@gmail.com>
2015-10-18 14:48:15 +0200

    Added PiWebcam Test

---
+++ PiWebcam.py
@@ -0,0 +1,40 @@
+# import the relevant libraries
+import time
+import os
+import pygame
+import pygame.camera
+from pygame.locals import *
+
+DEV_MODE = True
+
+if DEV_MODE ==  False:
+    os.putenv('SDL_FBDEV', '/dev/fb1')
+    os.putenv('SDL_MOUSEDRV', 'TSLIB')
+    os.putenv('SDL_MOUSEDEV', '/dev/input/touchscreen')
+
+sleeptime__in_seconds = 0.1
+
+# this is where one sets how long the script
+# sleeps for, between frames.sleeptime__in_seconds = 0.05
+# initialise the display window
+screen = pygame.display.set_mode([320, 240])
+pygame.mouse.set_visible(DEV_MODE)
+pygame.init()
+pygame.camera.init()
+# set up a camera object
+cam = pygame.camera.Camera("/dev/video0",(240, 320))
+# start the camera
+cam.start()
+
+while 1:
+
+    # sleep between every frame
+    time.sleep( sleeptime__in_seconds )
+    # fetch the camera image
+    image = cam.get_image()
+    # blank out the screen
+    screen.fill([0,0,0])
+    # copy the camera image to the screen
+    screen.blit( image, ( 0, 0 ) )
+    # update the screen to show the latest screen image
+    pygame.display.update()
---
+++ weatherTest.py
@@ -0,0 +1,6 @@
+import pywapi
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+result = pywapi.get_weather_from_yahoo('RSXX0199', 'metric')
+pp.pprint(result)
